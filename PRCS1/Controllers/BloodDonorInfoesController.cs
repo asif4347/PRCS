e3023d67798e30f33d0ba77e9043658096b494d8
Muhammad Asif <asif4347@icloud.com>
2016-11-17 13:28:22 -0800

    Added
    DD

---
+++ PRCS1/Controllers/BloodDonorInfoesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PRCS.Models;
+
+namespace PRCS1.Controllers
+{
+    public class BloodDonorInfoesController : Controller
+    {
+        private BloodDbContext db = new BloodDbContext();
+
+        // GET: BloodDonorInfoes
+        public ActionResult Index(string sName,string bloodGroup,string regNo)
+        {
+
+            var bloodLst = new List<string>();
+            var bloodQry = from d in db.BloodInfo
+                           orderby d.BloodGroup
+                           select d.BloodGroup;
+            bloodLst.AddRange(bloodQry.Distinct());
+            ViewBag.bloodGroup = new SelectList(bloodLst);
+
+
+            
+
+            var blood = from m in db.BloodInfo
+                        select m;
+
+            if (!String.IsNullOrEmpty(sName))
+            {
+                blood = blood.Where(s => s.Name.Contains(sName));
+            }
+            if (!string.IsNullOrEmpty(bloodGroup))
+            {
+                blood = blood.Where(x => x.BloodGroup == bloodGroup);
+            }
+            if (!String.IsNullOrEmpty(regNo))
+            {
+                blood = blood.Where(r => r.RegNo == regNo);
+            }
+
+
+
+            return View(blood);
+
+
+
+        }
+
+        // GET: BloodDonorInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BloodDonorInfo bloodDonorInfo = db.BloodInfo.Find(id);
+            if (bloodDonorInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bloodDonorInfo);
+        }
+
+        // GET: BloodDonorInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BloodDonorInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,RegNo,DonorNo,SerialNo,Name,SonOf,Gender,DOB,Weight,BloodGroup,LastDonation,NoOfDonation,Institute,Class,TelResidance,TelOffice,FAX,Mobile,Email")] BloodDonorInfo bloodDonorInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BloodInfo.Add(bloodDonorInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bloodDonorInfo);
+        }
+
+        // GET: BloodDonorInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BloodDonorInfo bloodDonorInfo = db.BloodInfo.Find(id);
+            if (bloodDonorInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bloodDonorInfo);
+        }
+
+        // POST: BloodDonorInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,RegNo,DonorNo,SerialNo,Name,SonOf,Gender,DOB,Weight,BloodGroup,LastDonation,NoOfDonation,Institute,Class,TelResidance,TelOffice,FAX,Mobile,Email")] BloodDonorInfo bloodDonorInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bloodDonorInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bloodDonorInfo);
+        }
+
+        // GET: BloodDonorInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BloodDonorInfo bloodDonorInfo = db.BloodInfo.Find(id);
+            if (bloodDonorInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bloodDonorInfo);
+        }
+
+        // POST: BloodDonorInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BloodDonorInfo bloodDonorInfo = db.BloodInfo.Find(id);
+            db.BloodInfo.Remove(bloodDonorInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PRCS1/Models/BloodDonorInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PRCS.Models
+{
+    public class BloodDonorInfo
+    {
+
+        public int ID { get; set; }
+        public string RegNo { get; set; }
+        public string DonorNo { get; set; }
+        public string SerialNo { get; set; }
+        
+        public string Name { get; set; }
+        [Display(Name ="S/o.D/o")]
+        public string SonOf { get; set; }
+        public string Gender { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        
+        public DateTime DOB { get; set; }
+        public float Weight { get; set; }
+
+        [Display(Name ="Blood Group")]
+        public string BloodGroup { get; set; }
+
+        [Display(Name = "Last Donation")]
+        [DataType(DataType.Date)]
+       
+        public DateTime LastDonation { get; set; }
+
+        [Display(Name = "No. of Donation")]
+        public int NoOfDonation { get; set; }
+        public string Institute { get; set; }
+        public string Class { get; set; }
+
+        [Display(Name = "Residance Telephone")]
+        public string TelResidance { get; set; }
+        [Display(Name = "Office Telephone")]
+        public string TelOffice { get; set; }
+        public string FAX { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+    }
+
+    public class BloodDbContext : DbContext
+    {
+
+        public DbSet<BloodDonorInfo> BloodInfo { get; set; }
+
+    }
+
+}
+
